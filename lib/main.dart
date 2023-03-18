0dfaa34b05ca5e88711c15eb332f30f388b4c94d
Salma Ahmed Mostafa <56895305+salma234-del@users.noreply.github.com>
2023-03-19 00:25:11 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Train D Edit',
+      title: 'Train D Edit Again',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
