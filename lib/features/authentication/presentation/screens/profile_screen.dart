e08a1869f2e719d803abe700a498aadb2366abf5
salma234-del <sa310107@gmail.com>
2023-03-26 16:20:27 +0200

    ss

--- lib/features/authentication/presentation/screens/profile_screen.dart
+++ lib/features/authentication/presentation/screens/profile_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:traind_app/core/utils/app_sizes.dart';
 import 'package:traind_app/core/utils/app_strings.dart';
 import 'package:traind_app/core/utils/components.dart';
