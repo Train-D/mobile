cb0bafd9b3062f56d7a04bd1b3a6c19a46e5be00
salma234-del <sa310107@gmail.com>
2023-04-12 17:26:02 +0200

    sign up entity

---
+++ lib/features/authentication/domain/entities/sign_up.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class SignUp extends Equatable {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+
+  const SignUp({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [
+        firstName,
+        lastName,
+        email,
+        password,
+      ];
+}
