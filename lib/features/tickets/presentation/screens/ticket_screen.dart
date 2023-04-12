344868071bf6ad228433b8e9b43d56c1830e782a
salma234-del <sa310107@gmail.com>
2023-04-12 16:18:53 +0200

    ..

--- lib/features/tickets/presentation/screens/ticket_screen.dart
+++ lib/features/tickets/presentation/screens/ticket_screen.dart
@@ -56,7 +56,7 @@ class TicketScreen extends StatelessWidget {
                           const SizedBox(
                             height: 30,
                           ),
-                       
+                        
                         ],
                       ),
                     ),
